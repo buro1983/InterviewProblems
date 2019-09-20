62860e7d690abaa56158eb0bf4290a2839fd2d69
Niloy Debnath <ndebnath@sandvine.com>
2019-09-20 04:55:32 -0400

    Added coin changing problem

--- DynamicProgramming/coinChangingAllSol.go
+++ DynamicProgramming/coinChangingAllSol.go
@@ -2,7 +2,7 @@
 https://www.geeksforgeeks.org/coin-change-dp-7/
 
 Given a value N, if we want to make change for N cents, and we have infinite supply of
-each of S = { S1, S2, .. , Sm} valued coins, how many ways can we make the change? The order of coins doesn’t matter.
+each of S = { S1, S2, .. , Sm} valued coins, how many ways can we make the change? The order of coins does not matter.
 For example, for N = 4 and S = {1,2,3}, there are four solutions: {1,1,1,1},{1,1,2},{2,2},{1,3}.
 So output should be 4. For N = 10 and S = {2, 5, 3, 6}, there are five solutions:
 {2,2,2,2,2}, {2,2,3,3}, {2,2,6}, {2,3,5} and {5,5}. So the output should be 5.
@@ -48,15 +48,9 @@ func numberOfSolution(total int, coins []int) int {
 	return T[len(coins)][total]
 }
 
-// print all possible combination for coin change
-func printCoinChangingSolution(l list.List, coins []int, total, pos int) {
-	if total == 0 {
-		fmt.Println()
-	}
-}
-
 func main() {
 	total := 15
 	coins := []int{3, 4, 6, 7, 9}
 	fmt.Println("numberOfSolution:", numberOfSolution(total, coins))
+
 }
